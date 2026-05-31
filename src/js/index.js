4ae9665a7921cb3e4c49833e00807362dbcd77b0
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-01 03:33:22 +0900

    style: import css in js

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import "../css/pages/index.css";
---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import "modern-normalize";
+import {
+  createIcons,
+  search,
+  user,
+  shoppingCart,
+  heart,
+  chevronFirst,
+  chevronLast,
+  chevronLeft,
+  chevronRight,
+} from "lucide";
+
+createIcons({
+  icons: {
+    search,
+    user,
+    shoppingCart,
+    heart,
+    chevronFirst,
+    chevronLast,
+    chevronLeft,
+    chevronRight,
+  },
+});
+import "../css/style.css";
