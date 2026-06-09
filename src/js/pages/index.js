5896f04c95bd9a2298e222891d60471b85b1f407
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-09 10:06:08 +0900

    feat: lazy loading 추가

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -36,5 +36,54 @@ async function heroFetch() {
   document.querySelector(".hero__image-3").src = products[3].thumbnail;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  let lazyloadImages;
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    const imageObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const image = entry.target;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(image => {
+      imageObserver.observe(image);
+    });
+  } else {
+    let lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }
+
+      lazyloadThrottleTimeout = setTimeout(() => {
+        const scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(img => {
+          if (img.offsetTop < window.innerHeight + scrollTop) {
+            img.src = img.dataset.src;
+            img.classList.remove("lazy");
+          }
+        });
+        if (lazyloadImages.length === 0) {
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+});
+
 heroFetch();
 renderTabs();
