0edf3d655ba23174af66d63f266a74f63be8368b
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-11 02:52:48 +0900

    fix: EventListener 제거?

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,13 @@ import "modern-normalize";
 import "../css/style.css";
 import { renderHeader } from "./modules/header.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  renderHeader();
-  const page = document.body.dataset.page;
+renderHeader();
+const page = document.body.dataset.page;
 
-  switch (page) {
-    case "main":
-      import("../css/pages/index.css");
-      import("./pages/index.js");
-      break;
-    case "404":
-      import("../css/pages/404.css");
-  }
-});
+switch (page) {
+  case "main":
+    import("./pages/index.js");
+    break;
+  case "404":
+    import("./pages/404.js");
+}
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -4,6 +4,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
+import "../../css/pages/index.css";
 import { renderTabs } from "../modules/tabs.js";
 
 const swiper = new Swiper(".swiper", {
