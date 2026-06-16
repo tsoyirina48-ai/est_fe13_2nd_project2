12a413e2533874cf08739ce74ba6582ef873f876
RaeChoe <picasomati@gmail.com>
2026-06-16 18:53:29 +0900

    feat: 탭 html css js 구현

--- src/js/pages/detail.js
+++ src/js/pages/detail.js
@@ -12,4 +12,26 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import "../../css/pages/detail.css";
 
-renderTabs();
+const tabs = document.querySelectorAll(".tabs a");
+const detailContents = document.querySelectorAll(".detail-content");
+
+tabs.forEach(t => {
+  t.addEventListener("click", e => {
+    e.preventDefault();
+
+    // 기존 active 제거
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
+    });
+    detailContents.forEach(dc => {
+      dc.classList.remove("active");
+    });
+
+    // 클릭한 탭 active 추가
+    t.classList.add("active");
+
+    // href 값을 가져온 후 active 추가
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).classList.add("active");
+  });
+});
